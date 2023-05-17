77e2a37dc739a4a68027884bad64ba68ce6ad2bf
emilioff8822 <emilioff@hotmail.it>
2023-05-17 15:33:49 +0200

    commit-css-12

--- atterraggio.php
+++ atterraggio.php
@@ -15,9 +15,41 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
+  <title>atterraggio</title>
 </head>
 <body>
-  <p>La tua password generata è: <?php echo $message; ?></p>
+<div class="container text-center">
+
+<h1>La tua password generata è:</h1> 
+<h2> <?php echo $message; ?></h2>
+
+
+</div>
 </body>
+
+<style>
+    body {
+      background: #c31432;  /
+      background: -webkit-linear-gradient(to right, #240b36, #c31432);  
+      background: linear-gradient(to right, #240b36, #c31432); 
+      color: #FFF;
+    }
+    .container {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      
+    }
+
+    h1 {
+      margin-bottom: 1rem;
+    }
+    h2 {
+      color: #00FF00; 
+    }
+  </style>
+
 </html>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 if(isset($_GET['length']) && !empty($_GET['length'])){
   $length = $_GET['length'];
   $password = generatePassword($length);
-  $_SESSION ['message'] = "La tua password e': $password";
+  $_SESSION ['message'] = "$password";
 
 
 header('Location: ./atterraggio.php');
@@ -18,15 +18,8 @@ exit;
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous'/>
-  <title>Generatore di password</title>
-</head>
+<?php require_once __DIR__ . '/partials/head.php' ?>
+
 <body>
 
 <div class="container text-center">
@@ -38,12 +31,50 @@ exit;
 
 </form>
 
-<p> <?php echo $message; ?> </p>
 
 
 </div>
 
-
+<style>
+    body {
+      background: #c31432;  /
+      background: -webkit-linear-gradient(to right, #240b36, #c31432);  
+      background: linear-gradient(to right, #240b36, #c31432); 
+      color: #FFF;
+    }
+    .container {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    form {
+    background: rgba(255,255,255,0.1);
+    padding: 60px;
+    border-radius: 15px;
+    box-shadow: 0px 0px 15px rgba(0,0,0,0.1);
+  }
+  input[type="number"] {
+    width: 100%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    margin-bottom: 10px;
+  }
+  input[type="submit"] {
+    background: #FFF;
+    color: #240b36;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background 0.3s ease;
+  }
+  input[type="submit"]:hover {
+    background: #240b36;
+    color: #FFF;
+  </style>
 
 </body>
 </html>
